ea1bfcae02d17ba0c45bb8d3c2e1bef43e383c2f
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2024-07-10 19:52:36 +0200

    add basic pokemon catalog api service implementation + typing

--- src/app/core/services/data-access-api/pokemon-catalog-api.service.ts
+++ src/app/core/services/data-access-api/pokemon-catalog-api.service.ts
@@ -1,12 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const API_VERSION: string = 'v2'
+const API_URL: string = `https://api.pokemontcg.io/${API_VERSION}`;
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonCatalogApiService {
 
-  constructor() { }
   constructor(private http: HttpClient) { }
 
+  getCards(): Observable<PokemonCatalogResponse<unknown>>{
+    return this.http.get<PokemonCatalogResponse<unknown>>(API_URL + '/cards');
+  }
+
+  getCard(id: string): Observable<PokemonCatalogResponse<unknown>>{
+    return this.http.get<PokemonCatalogResponse<unknown>>(API_URL + '/cards' + `/${id}`);
+  }
+
+  // --- TYPES ---
+  //TODO: initally load all of them at app start
+  getTypes(): Observable<PokemonCatalogResponse<Type>> { return this.http.get<PokemonCatalogResponse<Type>>(API_URL + '/types'); }
+
+  getSubTypes(): Observable<PokemonCatalogResponse<SubType>> { return this.http.get<PokemonCatalogResponse<SubType>>(API_URL + '/subtypes') }
+
+  getSuperTypes(): Observable<PokemonCatalogResponse<SuperType>> { return this.http.get<PokemonCatalogResponse<SuperType>>(API_URL + '/supertypes') }
+}
+
+
+type SuperType = string
+type SubType = string
+type Type = string;
+
+interface PokemonCatalogResponse<T> {
+  data: T[];
+  page?: number;
+  pageSize?: number;
+  count?: number;
+  totalCount?: number;
 }
