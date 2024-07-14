043ff466e7bdd9766e450e8ecc11666c14134f03
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2024-07-14 18:29:49 +0200

    fix typings

--- src/app/core/services/data-access-api/pokemon-catalog-api.service.ts
+++ src/app/core/services/data-access-api/pokemon-catalog-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Card } from '../../models/model';
 import { PokemonCatalogResponse, SearchParams, SubType, SuperType, Type } from '../../models/util-types';
 
 
@@ -13,16 +14,16 @@ export class PokemonCatalogApiService {
   private readonly API_VERSION: string = 'v2'
   private readonly API_URL: string = `https://api.pokemontcg.io/${this.API_VERSION}`;
 
-  getCards(): Observable<PokemonCatalogResponse<unknown>>{
-    return this.http.get<PokemonCatalogResponse<unknown>>(this.API_URL + '/cards');
+  getCards(): Observable<PokemonCatalogResponse<Card>>{
+    return this.http.get<PokemonCatalogResponse<Card>>(this.API_URL + '/cards');
   }
 
-  getCard(id: string): Observable<PokemonCatalogResponse<unknown>>{
-    return this.http.get<PokemonCatalogResponse<unknown>>(this.API_URL + '/cards' + '/' + `${id}`);
+  getCard(id: string): Observable<PokemonCatalogResponse<{data:Card}>>{
+    return this.http.get<PokemonCatalogResponse<{data: Card}>>(this.API_URL + '/cards' + '/' + `${id}`);
   }
 
-  searchCards(searchParams: SearchParams):Observable<PokemonCatalogResponse<unknown>>{
-    return this.http.get<PokemonCatalogResponse<unknown>>(this.API_URL + '/cards', );
+  searchCards(searchParams: SearchParams):Observable<PokemonCatalogResponse<Card>>{
+    return this.http.get<PokemonCatalogResponse<Card>>(this.API_URL + '/cards', );
   }
 
   // --- TYPES ---
