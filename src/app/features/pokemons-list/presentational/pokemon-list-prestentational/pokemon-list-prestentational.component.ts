28ad03db80347c8192f545c6e3144115fc2e7584
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2024-07-14 11:03:12 +0200

    add basic paginator functionality

--- src/app/features/pokemons-list/presentational/pokemon-list-prestentational/pokemon-list-prestentational.component.ts
+++ src/app/features/pokemons-list/presentational/pokemon-list-prestentational/pokemon-list-prestentational.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
 
@@ -15,6 +15,8 @@ import { MatTableModule } from '@angular/material/table';
   styleUrl: './pokemon-list-prestentational.component.scss'
 })
 export class PokemonListPrestentationalComponent {
-@Input() dataSource: any;
-@Input() displayedColumns: string[] = ['id', 'name', 'supertype'];
+  @Input() dataSource: any;
+  @Input() displayedColumns: string[] = ['id', 'name', 'supertype', 'types', 'subtypes'];
+
+  @Output() paginationData: EventEmitter<PageEvent> = new EventEmitter()
 }
