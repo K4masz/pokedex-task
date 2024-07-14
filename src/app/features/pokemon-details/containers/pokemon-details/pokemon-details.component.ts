5d51fbb208b4f7b3c65db18378084544d2f362a9
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2024-07-15 01:17:42 +0200

    introduce new design for pokemon details

--- src/app/features/pokemon-details/containers/pokemon-details/pokemon-details.component.ts
+++ src/app/features/pokemon-details/containers/pokemon-details/pokemon-details.component.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { Card } from '../../../../core/models/model';
+import { SubTypesFacade } from '../../../../core/state/subtypes/sub-types.facade';
+import { SuperTypesFacade } from '../../../../core/state/supertypes/super-types.facade';
+import { TypesFacade } from '../../../../core/state/types/types.facade';
 import { PokemonDetailsPresentationalComponent } from "../../presentational/pokemon-details-presentational/pokemon-details-presentational.component";
 
 @Component({
@@ -12,6 +16,15 @@ import { PokemonDetailsPresentationalComponent } from "../../presentational/poke
   styleUrl: './pokemon-details.component.scss'
 })
 export class PokemonDetailsComponent {
-http = inject(HttpClient);
-data$: Observable<object> = this.http.get<{data:object}>('https://api.pokemontcg.io/v2/cards/ecard2-H2').pipe(map((x=> x.data ?? {})))
+  http = inject(HttpClient);
+
+  typesFacade = inject(TypesFacade);
+  superTypesFacade = inject(SuperTypesFacade);
+  subTypesFacade = inject(SubTypesFacade);
+
+  types$: Observable<string[]> = this.typesFacade.types$;
+  superTypes$: Observable<string[]> = this.superTypesFacade.superTypes$;
+  subTypes$: Observable<string[]> = this.subTypesFacade.subTypes$;
+
+  data$: Observable<Card> = this.http.get<{data: Card}>('https://api.pokemontcg.io/v2/cards/ecard2-H2').pipe(map((x => x.data)))
 }
--- src/app/features/pokemon-details/presentational/pokemon-details-presentational/pokemon-details-presentational.component.ts
+++ src/app/features/pokemon-details/presentational/pokemon-details-presentational/pokemon-details-presentational.component.ts
@@ -1,13 +1,75 @@
-import { Component, Input } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
-
+import { Card } from '../../../../core/models/model';
+import { SubType, SuperType, Type } from '../../../../core/models/util-types';
 @Component({
   selector: 'app-pokemon-details-presentational',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatChipsModule, MatTableModule, JsonPipe, ReactiveFormsModule, MatFormFieldModule, MatSelectModule, MatInputModule],
   templateUrl: './pokemon-details-presentational.component.html',
-  styleUrl: './pokemon-details-presentational.component.scss'
+  styleUrl: './pokemon-details-presentational.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PokemonDetailsPresentationalComponent {
-  @Input() data!: any | null;
+  formBuilder = inject(FormBuilder)
+
+  @Input() set card(value: Card | null) { //TODO: refactor
+    if (value){
+      this.populateForm(value)
+  }
+    this._card = value;
+  };
+  _card!: Card | null;
+  get card(): Card | null {
+    return this._card
+  }
+  @Input() similarCards: Card[] = [];
+  @Input() attacksTableDisplayedColumns: string[] = ['name', 'damage', 'cost', 'convertedEnergyCost'];
+
+  @Input() superTypes: SuperType[] = [];
+  @Input() types: Type[] = [];
+  @Input() subTypes: SubType[] = [];
+
+  @Output() editedCardData = new EventEmitter<Partial<Card>>();
+
+  editMode = false;
+
+  cardForm = this.formBuilder.group<Partial<Card>>({
+    name: '',
+    hp: '',
+    supertype: '',
+    types: [],
+    subtypes: []
+  },);
+
+  private populateForm(value: Partial<Card>){
+    this.cardForm.patchValue(value);
+    this.cardForm.disable();
+  }
+
+  edit(){
+    this.editMode = true;
+    this.cardForm.enable();
+  }
+
+  save(){
+    this.editMode = false;
+    this.cardForm.disable();
+    console.log(this.cardForm.value) //TODO:Remove
+    this.editedCardData.emit(this.cardForm.value as Partial<Card>);
+  }
+
+  discard(){
+    if(this._card)
+    this.populateForm(this._card);
+  }
+
+
 }
